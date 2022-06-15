1dcf530c39b29e6c9b2538475d49c45186093db1
ayeshoali <ayeshoali@gmail.com>
2022-06-15 17:55:19 +0500

    ...

--- validation.js
+++ validation.js
@@ -1,5 +1,5 @@
 let validateEmail = (email) => {
-    if (email.includes("@") && email.includes(".com")) {
+    if (email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
         return true;
     }
     return false;
@@ -78,9 +78,10 @@ form.addEventListener("submit", (e) => {
     //     console.log("else");
     //     form.setAttribute("action", "./Welcom.html");
     // }
-    // if (flag) {
-    //     form.setAttribute("action", "./Welcom.html");
-    // }
+    if (flag) {
+        window.location = './Welcom.html';
+        form.setAttribute("action", "./Welcom.html");
+    }
 
     // form.setAttribute("action", "./Welcom.html");
 })
